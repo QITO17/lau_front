b52dde8632e6578ec33f31ed9775df5a15074bd3
Jostin Arley Hurtado <113543832+QITO17@users.noreply.github.com>
2025-02-06 17:51:46 -0500

    Update Servicios2.jsx

--- src/components/Servicios2.jsx
+++ src/components/Servicios2.jsx
@@ -27,7 +27,7 @@ const Servicio2 = ({
     const fechaFormateada = fechaActual.toISOString().split('T')[0];
     let fecha = fechaFormateada;
     const fechaHora = new Date();
-    const horaFormateada = fechaHora.toLocaleTimeString();
+   const horaFormateada = fechaHora.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
     let hora = horaFormateada
     console.log('Holita')
     console.log('Hola soy hora ', hora)
